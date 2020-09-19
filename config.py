e2ccf86f7abf82106e90495a8d6ac8fdfc4dbfe4
Drew <andrew.s.karn@gmail.com>
2020-09-18 23:59:54 -0700

    Use Heroku db

--- config.py
+++ config.py
@@ -8,8 +8,6 @@ DEBUG = True
 
 # Connect to the database
 
-
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgres://sozukvyiradwfz:807483e334f9ce8e0b9c1de753539a04b1d6ece79ffd82d306cbc54ea936706e@ec2-54-86-57-171.compute-1.amazonaws.com:5432/d1pae93kccdp8j'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 TEMPLATES_AUTO_RELOAD=True
